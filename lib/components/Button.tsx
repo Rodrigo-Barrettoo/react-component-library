28699821fb1f4360b101e51fa80a392d3ebbaa2a
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2024-08-05 13:05:51 -0300

    clean project and adjust in button component

--- lib/components/Button.tsx
+++ lib/components/Button.tsx
@@ -1,5 +1,9 @@
-const Button = () => {
-  return <button className="bg-red-300 text-red-900">Button</button>;
+import { ButtonHTMLAttributes } from "react";
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button = ({ ...props }: ButtonProps) => {
+  return <button className="bg-red-300 text-red-900" {...props} />;
 };
 
 export default Button;
